9389568450ef61d46ddcdc11a9e266490e148955
Egon Paeppler <paeppler@web.de>
2023-11-15 19:06:16 +0100

    finish CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
   root "articles#index"
 
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show" -> become
 
-  get "/articles", to: "articles#index"
+  resources :articles
+
+
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
